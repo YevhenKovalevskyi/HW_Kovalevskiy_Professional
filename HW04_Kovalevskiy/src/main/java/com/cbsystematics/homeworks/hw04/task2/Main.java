6ab7992cfe01214357f7f703a29a5ecd49259eb1
e.kovalevskiy <carsar.sd@gmail.com>
2020-10-26 23:33:08 +0200

    home work 4

---
+++ HW04_Kovalevskiy/src/main/java/com/cbsystematics/homeworks/hw04/task2/Main.java
@@ -0,0 +1,16 @@
+package com.cbsystematics.homeworks.hw04.task2;
+
+/**
+ * Main Class represents homework #4 #task2
+ *
+ * @author e.kovalevskiy
+ * @version 1.0
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        /*
+         - Если я правильно понял, то это задание сделано в taskExtra
+        */
+    }
+}
